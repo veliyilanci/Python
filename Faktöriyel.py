e81953cde538a4ec7b0eac11639c150176798bd3
Veli Yılancı <veliyilanci@gmail.com>
2020-11-21 15:48:49 +0300

    Update Faktöriyel.py

--- Faktöriyel.py
+++ Faktöriyel.py
@@ -1,11 +1,11 @@
 def faktöriyel(sayı):
     faktöriyel = 1
     if sayı == 0 or sayı == 1:
-        print(faktöriyel)
+        return(faktöriyel)
     if sayı <0:
         print("Lütfen pozitif bir doğal sayı giriniz!")
     else:
         while sayı >= 1:
             faktöriyel = faktöriyel*sayı
             sayı =sayı- 1
-        print(faktöriyel)
+        return(faktöriyel)
