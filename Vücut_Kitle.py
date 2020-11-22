ee60ff4bcc4c3086697ebc75e913afff39738ceb
Veli Yılancı <veliyilanci@gmail.com>
2020-11-22 23:18:48 +0300

    Update Vücut_Kitle.py

--- Vücut_Kitle.py
+++ Vücut_Kitle.py
@@ -4,9 +4,9 @@ def vki(boy,kilo):
   endeks=kilo/(boy**2)
   if endeks <= 18.49:
     print("İdeal kilonun altındasınız. Vücut kitle indeksiniz:", endeks)
-  elif endeks > 18.5 and endeks <=24.99 :
+  elif endeks => 18.5 and endeks <=24.99 :
     print("İdeal kiloya sahipsiniz. Vücut kitle indeksiniz:", endeks)
-  elif endeks > 25 and endeks <=29.99:
+  elif endeks => 25 and endeks <=29.99:
     print("İdeal kilonun üstündesiniz. Vücut kitle indeksiniz:", endeks)
-  elif endeks > 30:
+  elif endeks => 30:
     print("Boyunuza göre vücut ağırlığınız fazladır. Obez sınıfına giriyorsunuz. Vücut kitle indeksiniz:", endeks)
